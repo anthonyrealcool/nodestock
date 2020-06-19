41811dba47421ceff79b7fc2313a314d825f50e1
Anthony Cool <vandeant11@gmail.com>
2020-06-19 12:02:28 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//Set Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "Hellewwow ther, this is otherr stuff, you dick me!!"
+
+//Set handlebars Routes
+app.get('/', function(req,res){
+  res.render('home', {
+    stuff: otherstuff
+  });
+})
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(PORT, ()=> console.log('Sever Listening on port' + PORT));
