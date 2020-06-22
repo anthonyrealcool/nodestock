1383602cc349ab6fefccf6e179b8520975918499
Anthony Cool <vandeant11@gmail.com>
2020-06-22 18:59:59 +0700

    update for search bar to any api listed

--- index.js
+++ index.js
@@ -1,31 +1,75 @@
-//Stock Market Portfolio App by Anthony T. Cool
-
+// Stock Market Portfolio App By Anthony T. Cool
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
-//Set Handlebars Middleware
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+// API KEY pk_062031d20883444f9ea74e2610fe2011
+// create call_api function
+function call_api(finishedAPI, ticker) {
+  request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_062031d20883444f9ea74e2610fe2011', {
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    if (res.statusCode === 200) {
+      //console.log(body);
+      finishedAPI(body);
+    };
+  });
+};
+
+
+// Set Handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-const otherstuff = "Hellewwow ther, this is otherr stuff, you dick me!!"
+const otherstuff = "hello there, this is other stuff!";
 
-//Set handlebars Routes
-app.get('/', function(req,res){
-  res.render('home', {
-    stuff: otherstuff
-  });
-})
+// Set handlebar index GET route
+app.get('/', function (req, res) {
+  call_api(function (doneAPI) {
+    res.render('home', {
+      stock: doneAPI,
+    });
+  }, "fb");
 
-//Create about page route
+});
+
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+  call_api(function (doneAPI) {
+    //console.log(doneAPI);
+    //posted_stuff = req.body.stock_ticker;
+    res.render('home', {
+      stock: doneAPI,
+    });
+  }, req.body.stock_ticker);
+
+});
+
+
+
+
+// create about page route
 app.get('/about.html', function (req, res) {
   res.render('about');
-})
+});
 
-//Set Static Folder
-app.use(express.static(path.join(__dirname,'public')));
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, ()=> console.log('Sever Listening on port' + PORT));
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
